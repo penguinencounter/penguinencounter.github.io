b25c99a4233dd4bd2033a2a2b8a3a8fd2a1a1b14
Miles <miles.h.lin@gmail.com>
2020-06-03 13:47:23 -0700

    2020-06-03 13:47:23 autocommit

---
+++ compus.sh
@@ -0,0 +1,8 @@
+echo Commiting as $(date '+%F %H:%M:%S') autocommit
+git add * &> /dev/null
+git commit -m "$(date '+%F %H:%M:%S') autocommit" &> /dev/null
+echo Commited
+git push &> /dev/null
+echo Pushed
+echo Done
+
