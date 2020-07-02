2229761e77f68e4d3e522f36f2b9013eb64429ee
Miles <miles.h.lin@gmail.com>
2020-07-02 10:07:46 -0700

    2020-07-02 10:07:46 autocommit

---
+++ nbtexplorerweb/scripts/base.js
@@ -0,0 +1,42 @@
+var reader, data;
+var onLoad = function() {
+    var inputElement = document.getElementById("importer");
+    var button = document.getElementById("impbutton");
+    console.log(inputElement);
+    inputElement.addEventListener("change", handleFiles, false);
+    button.addEventListener("click", beginUpload)
+}
+var handleFiles = function () {
+    var resultElement = document.getElementById("result");
+    var file = this.files[0];
+    console.log(file);
+    reader = new FileReader();
+    reader.onload = (function(element) {return function(e) {data = e.target.result;setTimeout(completeUpload, 10)}; })(resultElement);
+    reader.readAsBinaryString(file);
+}
+var beginUpload = function (e) {
+    var inputElement = document.getElementById("importer");
+    e.target.value = "Waiting for file..."
+    e.target.className = "working";
+    inputElement.click();
+}
+var completeUpload = function () {
+    var e = document.getElementById("impbutton");
+    e.value = "File uploaded";
+    e.className = "working";
+    document.getElementById("result").innerHTML = data;
+    console.log(data.decodeEscapeSequence())
+    setTimeout(resetButton, 3000);
+}
+var resetButton = function() {
+    var e = document.getElementById("impbutton");
+    e.value = "Ready - click to import";
+    e.className = "idle";
+}
+
+
+String.prototype.decodeEscapeSequence = function() {
+    return this.replace(/\\x([0-9A-Fa-f]{2})/g, function() {
+        return String.fromCharCode(parseInt(arguments[1], 16));
+    });
+};
