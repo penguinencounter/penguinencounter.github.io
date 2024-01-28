10a41ce5f64ef1f91ec017205bb9957c41dfde37
penguinencounter <49845522+penguinencounter@users.noreply.github.com>
2024-01-27 20:06:36 -0800

    [skip ci] WIP

--- transmute.py
+++ transmute.py
@@ -6,16 +6,18 @@ import shutil
 from glob import glob
 from tempfile import TemporaryDirectory
 import contextlib
-from typing import NoReturn
+from typing import Literal, NamedTuple, NoReturn, TypeAlias
 import logging
 from rich.logging import RichHandler
 from hashlib import sha256
+import importlib
+import importlib.util
 
 logging.basicConfig(level=logging.DEBUG, format="%(name)s: %(message)s", handlers=[RichHandler()])
 log = logging.getLogger("core")
 
 def stop(reason: str) -> NoReturn:
-    print(f'Cannot continue: {reason}', file=sys.stderr)
+    log.fatal(f'{reason}')
     exit(1)
 
 
@@ -47,6 +49,78 @@ def prepare_env(sources: list[str]):
         yield tmpdir
 
 
+class PluginPipelineInfo(NamedTuple):
+    target: str
+
+    @classmethod
+    def load(cls, template: dict):
+        return cls(
+            "file"
+        )
+
+
+class AboutPlugin(NamedTuple):
+    name: str
+    provides: set[str]
+    use: list[str]
+    path: str
+    pipeline: PluginPipelineInfo
+
+    @classmethod
+    def load(cls, name: str, template: dict):
+        log.debug("Constructing plugin data for %s", name)
+        provides = template.get("provides", [])
+        provide_lst: set[str] = set()
+        if isinstance(provides, str):
+            provide_lst.add(provides)
+        elif isinstance(provides, list):
+            provide_lst.update(provides)
+        else:
+            stop(f"While loading plugin {name} info: 'provides' is not list or string or nothing (actually {provides=})")
+        provide_lst.add(name)
+        use = template.get("use", [])
+        if not isinstance(use, list):
+            stop(f"While loading plugin {name} info: 'use' is not list or nothing (actually {use=})")
+        path = template.get("path")
+        if path is None:
+            stop(f"While loading plugin {name} info: no source path")
+
+        pipeline_data = template.get("pipeline")
+        if pipeline_data is None:
+            stop(f"While loading plugin {name} info: no pipeline info")
+        elif not isinstance(pipeline_data, dict):
+            stop(f"While loading plugin {name} info: 'pipeline' is not table (actually {pipeline_data=})")
+        return cls(
+            name = name,
+            provides = provide_lst,
+            use = use,
+            path = path,
+            pipeline = PluginPipelineInfo.load(pipeline_data)
+        )
+
+
+
+def load_plugin(about_plugin: AboutPlugin):
+    hashn = sha256(about_plugin.name.encode()).hexdigest()
+    full_name = f"_transmute_generated.plugins.{hashn}"
+    importspec = importlib.util.spec_from_file_location(full_name, about_plugin.path)
+    if importspec is None:
+        log.error("Cannot load a plugin from %s", about_plugin.path)
+
+
+def check_consistency(plugin_list: list[AboutPlugin]):
+    available_names = set()
+    requested_names = set()
+    for plugin in plugin_list:
+        available_names.update(plugin.provides)
+        requested_names.update(plugin.use)
+    missing = requested_names - available_names
+    if len(missing) > 0:
+        stop(f"Plugin consistency error: no plugin can meet requirements: {missing}")
+    else:
+        log.info(f"plugin consistency check passed")
+
+
 def main():
     conf_path = os.environ.get("TRANSMUTE_CONF", "transmute.toml")
     if not os.path.exists(conf_path):
@@ -71,6 +145,13 @@ def main():
     if not engine or not os.path.exists(engine):
         stop(f"Cannot find Python interpreter. Guessed {engine if engine else '(failed to retrieve)'}. Set PYTHON_EXE to the path of a Python interpreter.")
 
+    raw_plugins = conf.get("plugins", {})
+    if not isinstance(raw_plugins, dict):
+        stop(f"Invalid type: 'plugins' should be a dict")
+    plugins = [AboutPlugin.load(k, v) for k, v in raw_plugins.items()]
+    check_consistency(plugins)
+    log.info("%d plugins ready", len(raw_plugins))
+
     with prepare_env(sources) as presrc:
         # Pre-process
         pre_conf = conf.get("preprocess", {})
@@ -82,12 +163,9 @@ def main():
             if not isinstance(pre_use, list):
                 stop(f"Invalid type: preprocess.use should be list, is actually {type(pre_use)}")
             for pre in pre_use:
-                pure = os.path.abspath(os.path.expanduser(pre))
-                hashname = sha256(pure.encode()).hexdigest()
-                log.debug("Using %s (%s...)", pre, hashname[:16])
+                ...
                 
 
 
 if __name__ == "__main__":
     main()
-    exit(1)
