e6848bdb7ea2c8ac95bb3ff58ba813c3a5136d61
penguinencounter <49845522+penguinencounter@users.noreply.github.com>
2024-06-05 10:37:34 -0700

    yes this is correct enough

--- build_variants.py
+++ build_variants.py
@@ -1,12 +1,28 @@
+from __future__ import annotations
+
 import os
 import re
 import shutil
 import tempfile
 from pathlib import Path
-from typing import Literal, Callable, NamedTuple
+from typing import Literal, NamedTuple, Protocol
 
 import bs4
 from bs4 import Tag
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+from rich import print as rp
+from rich.progress import (
+    BarColumn,
+    MofNCompleteColumn,
+    Progress,
+    TaskID,
+    TaskProgressColumn,
+    TextColumn,
+    TimeElapsedColumn,
+    TimeRemainingColumn,
+)
+
+jinja = Environment(loader=FileSystemLoader("src"), autoescape=select_autoescape())
 
 
 class MapProvider:
@@ -78,82 +94,103 @@ SOURCES = [
 ]
 
 
+class ProcessingAction:
+    KEEP = 0
+    DELETE = -1
+    REPLACE = -2
+
+
+class Attachments:
+    def __init__(self, build_script: BuildScript):
+        self.build_script: BuildScript = build_script
+
+
+class FileAttachments(Attachments):
+    def __init__(self, build_script: BuildScript):
+        super().__init__(build_script)
+        self.soup: bs4.BeautifulSoup | None = None
+        self.soup_modified: bool = False
+
+
+class FileActionType(Protocol):
+    def __call__(self, path: Path, attachments: FileAttachments): ...
+
+
+class ProjectActionType(Protocol):
+    def __call__(
+        self, path: Path, attachments: Attachments, progress: Progress = None, task: TaskID = None
+    ): ...
+
+
 class BuildScript(NamedTuple):
     name: str
-    target: Callable[[Path], None]
+    targets: list[
+        tuple[Literal["file"], FileActionType] | tuple[Literal["project"], ProjectActionType]
+    ]
     mount: str
 
 
-def prepare(target: Path):
+def prepare(target: Path, progress: Progress = None, prog_task: TaskID = None):
     copies = 0
     filec = 0
     validates = 0
-    for base, dirs, files in os.walk("."):
+
+    total = 0
+    for base, dirs, files in os.walk(".", followlinks=True):
+        total += len(files)
+    progress.update(prog_task, total=total)
+
+    for base, dirs, files in os.walk(".", followlinks=True):
         pb = Path(base)
         for file in files:
             filec += 1
+            if progress is not None and prog_task is not None:
+                progress.advance(prog_task, 1)
             for source in SOURCES:
                 validates += 1
                 state, result = source.process(pb / file, target)
                 if state:
                     k: Path = result
-                    print(
-                        f"\rcopy {str(pb / file)[-30:]} -> {str(k)[-30:]}".ljust(70),
-                        end="",
-                        flush=True,
-                    )
                     os.makedirs(k.parent, exist_ok=True)
                     shutil.copy(pb / file, k)
                     copies += 1
-                    print(
-                        f"\rdone {str(pb / file)[-30:]} -> {str(k)[-30:]}".ljust(70),
-                        end="",
-                        flush=True,
-                    )
                     break
                 elif state is None:
                     continue
                 elif not state:
                     break
-    print(
-        f"\rCopy completed ({copies} items): checked {filec} files / {validates} validations.".ljust(
-            100
-        ),
-        flush=True,
-    )
+    rp(rf"[green]Copied [bold]{copies}[/] files to [bold cyan]{target}[/][/]")
 
 
-def variant_relatives(page: bs4.BeautifulSoup, build: BuildScript):
+def fix_rel_url(path: Path, att: FileAttachments):
+    if path.suffix != ".html":
+        return
+    page = att.soup
+    build = att.build_script
     if build.mount == "":
         return
 
     def is_local(a: Tag):
         return "data-link-absolute" not in a.attrs
 
+    modified = False
+
     for result in filter(is_local, page.select("[href], [src]")):
         result: Tag
-        if result.name == 'script':
+        if result.name == "script":
             continue
-        if result.name == 'link' and 'rel' in result.attrs and 'stylesheet' in result.attrs['rel']:
+        if result.name == "link" and "rel" in result.attrs and "stylesheet" in result.attrs["rel"]:
             continue
         if "href" in result.attrs:
             if result.attrs["href"].startswith("/") and not result.attrs["href"].startswith("//"):
                 result.attrs["href"] = "/" + build.mount + result.attrs["href"]
+                modified = True
         if "src" in result.attrs:
             if result.attrs["src"].startswith("/") and not result.attrs["src"].startswith("//"):
                 result.attrs["src"] = "/" + build.mount + result.attrs["src"]
-
-
-def resolve_relative_uris(base: Path, build: BuildScript):
-    for path, dirs, files in base.walk():
-        for file in files:
-            if not file.endswith(".html"):
-                continue
-            with open(path / file) as f:
-                soup = bs4.BeautifulSoup(f.read(), "html.parser")
-            variant_relatives(soup, build)
-            with open(path / file, "w") as f:
-                f.write(soup.decode())
+                modified = True
+    if modified:
+        att.soup_modified = True
 
 
 def process_variant_desc(page: bs4.BeautifulSoup, target_name: str) -> bool:
@@ -181,6 +218,19 @@ def process_variant_desc(page: bs4.BeautifulSoup, target_name: str) -> bool:
         return target_name in results
 
 
+def noscript_v2(target: Path, attach: FileAttachments):
+    if target.suffix == ".js":
+        target.unlink()
+    if target.suffix != ".html":
+        return
+    struct = attach.soup
+    attach.soup_modified = True
+    for e in struct.find_all("script"):
+        e.decompose()
+    for e in struct.select('[data-js-required]'):
+        e.decompose()
+
+
 def process_noscript(target: Path, force: bool = False):
     with open(target) as f:
         content = f.read()
@@ -189,12 +239,12 @@ def process_noscript(target: Path, force: bool = False):
         return None
     for e in struct.find_all("script"):
         e.decompose()
-    for e in struct.find_all(attrs={"data-js-required"}):
+    for e in struct.select('[data-js-required]'):
         e.decompose()
     return struct
 
 
-def noscript(target: Path):
+def noscript(target: Path, _: Attachments, progr: Progress, task: TaskID):
     shutil.rmtree(target / "dist", ignore_errors=True)
     targets = []
     for base, dirs, files in os.walk(target):
@@ -203,6 +253,7 @@ def noscript(target: Path):
                 targets.append(Path(base) / file)
             elif file.endswith(".js"):
                 (Path(base) / file).unlink()
+    progr.update(task, total=len(targets))
     missing_template = process_noscript(target / "var_unavailable.html", force=True)
     for target in targets:
         struct = process_noscript(target)
@@ -210,6 +261,7 @@ def noscript(target: Path):
             struct = missing_template
         with open(target, "w") as f:
             f.write(struct.decode())
+        progr.advance(task, 1)
 
 
 def full(target: Path):
@@ -217,20 +269,86 @@ def full(target: Path):
 
 
 if __name__ == "__main__":
-    builds = [BuildScript("nojs", noscript, "v/nojs"), BuildScript("full", full, "")]
+    builds = [
+        BuildScript("nojs", [("file", noscript_v2), ("file", fix_rel_url), ("project", noscript)], "v/nojs"),
+        BuildScript("full", [("file", fix_rel_url)], ""),
+    ]
     output = Path("deploy")
     shutil.rmtree(output, ignore_errors=True)
     os.makedirs(output)
     for script in builds:
-        name, processor, mount = script.name, script.target, script.mount
-        with tempfile.TemporaryDirectory() as td:
+        name = script.name
+        mount = script.mount
+        with tempfile.TemporaryDirectory() as td, Progress(
+            TextColumn("[progress.description]{task.description}"),
+            BarColumn(),
+            TaskProgressColumn(),
+            MofNCompleteColumn(),
+            TimeElapsedColumn(),
+            TimeRemainingColumn(),
+            refresh_per_second=20,
+        ) as prog:
+            copy_task = prog.add_task("[bright_blue]Stage build env[/]", total=None)
+            tasks = []
+            for action in script.targets:
+                tasks.append(
+                    prog.add_task(
+                        rf"[italic magenta]{action[1].__name__}[/]", total=None, start=False
+                    )
+                )
+            mount_task = prog.add_task("[bright_green]Mount result[/]", total=1, start=False)
+
             p = Path(td)
-            print(f"building {name}")
-            prepare(p)
-            resolve_relative_uris(p, script)
-            processor(p)
+            rp(f"Building [bold green]{name}[/]")
+            prepare(p, prog, copy_task)
+
+            idx = 0
+            queue = script.targets.copy()
+
+            while 1:
+                if len(queue) == 0:
+                    break
+                leader = queue.pop(0)
+                this_batch = [leader]
+                if leader[0] == "file":
+                    while len(queue) > 0:
+                        mode, _ = next_item = queue[0]
+                        if mode == "file":
+                            this_batch.append(queue.pop(0))
+                        else:
+                            break
+                    paths = []
+                    for path, dirs, files in p.walk(follow_symlinks=True):
+                        paths.extend(map(lambda k: path / k, files))
+                    for x in range(idx, idx + len(this_batch)):
+                        prog.start_task(tasks[x])
+                        prog.update(tasks[x], total=len(paths))
+                    attach = FileAttachments(script)
+                    for path in paths:
+                        for i, (_, process) in enumerate(this_batch):
+                            if not (path.exists() and path.is_file()):
+                                prog.advance(tasks[idx + i])
+                                continue
+                            with open(path, "rb") as f:
+                                content = f.read()
+                            if path.suffix == ".html":
+                                attach.soup = bs4.BeautifulSoup(content, "html.parser")
+                            process(path, attach)
+                            if attach.soup_modified:
+                                with open(path, "w") as f:
+                                    f.write(attach.soup.decode())
+                            prog.advance(tasks[idx + i])
+                else:
+                    prog.start_task(tasks[idx])
+                    attach = Attachments(script)
+                    _, process = leader
+                    process(p, attach, prog, tasks[idx])
+                idx += len(this_batch)
+
             module_out = output / mount
             if module_out.exists():
-                print(f"[WARNING] clobbering {module_out} - check mount points")
+                rp(f"\\[WARNING] clobbering {module_out} - check mount points")
             module_out.mkdir(parents=True, exist_ok=True)
+            prog.start_task(mount_task)
             shutil.copytree(p, module_out, dirs_exist_ok=True)
+            prog.advance(mount_task)
