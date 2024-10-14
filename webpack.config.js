9ea8c0c04544e431800d7a5254a8d7ea4f7510e9
penguinencounter <49845522+penguinencounter@users.noreply.github.com>
2024-10-14 13:21:42 -0700

    use pnpm, upgrade deps

--- webpack.config.js
+++ webpack.config.js
@@ -51,7 +51,13 @@ const config = {
             },
             {
                 test: /\.s[ac]ss$/i,
-                use: [stylesHandler, 'css-loader', 'sass-loader'],
+                use: [stylesHandler, 'css-loader', {
+                    loader: 'sass-loader',
+                    options: {
+                        api: 'modern',
+                        sassOptions: {}
+                    }
+                }],
             },
             {
                 test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
